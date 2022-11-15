cf8467a475648570986222a6291680b71c6473e6
Danilo Bonomo <danilo.bonomo@outlook.com>
2022-11-15 18:40:23 +0100

    Graphical improvments

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
+import { LoginComponent } from './login/login.component';
+import { SigninComponent } from './signin/signin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SigninComponent
   ],
   imports: [
     BrowserModule,
